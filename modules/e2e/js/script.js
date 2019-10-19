f11587c161f90c1d8baa3503bd45a947f0c79620
Uwe Gerdes <entwicklung@uwegerdes.de>
2019-10-19 12:54:06 +0200

    extracting widgets from index.pug and layout.less

--- modules/e2e/js/script.js
+++ modules/e2e/js/script.js
@@ -1,11 +1,10 @@
 /**
  * scripts for e2e-workflow
  */
-'use strict'
 
-/* global XMLHttpRequest */
+'use strict';
 
-let handler = {}
+let handler = {};
 
 /**
  * toggle element by id
@@ -14,24 +13,24 @@ handler['data-toggle'] = {
   elements: [window],
   event: 'load',
   func: () => {
-    const elements = document.querySelectorAll('[data-toggle]')
+    const elements = document.querySelectorAll('[data-toggle]');
     elements.forEach((element) => {
-      const toggleList = document.querySelectorAll(element.dataset.toggle)
+      const toggleList = document.querySelectorAll(element.dataset.toggle);
       element.addEventListener('click', () => {
         toggleList.forEach((toggled) => {
-          toggled.classList.toggle('hidden')
-        })
-      })
+          toggled.classList.toggle('hidden');
+        });
+      });
       toggleList.forEach((toggled) => {
         toggled.childNodes.forEach((child) => {
           child.addEventListener('click', (event) => {
-            event.stopPropagation()
-          })
-        })
-      })
-    })
+            event.stopPropagation();
+          });
+        });
+      });
+    });
   }
-}
+};
 
 /**
  * open link
@@ -40,41 +39,27 @@ handler['data-xhr'] = {
   elements: document.querySelectorAll('[data-xhr]'),
   event: 'click',
   func: function (event) {
-    const element = event.target
-    const response = document.querySelectorAll(element.dataset.xhrResponse)[0]
-    response.innerHTML = ''
-    const xhttp = new XMLHttpRequest()
-    let seenBytes = 0
+    const element = event.target;
+    const response = document.querySelectorAll(element.dataset.xhrResponse)[0];
+    response.innerHTML = '';
+    const xhttp = new XMLHttpRequest();
+    let seenBytes = 0;
     xhttp.onreadystatechange = function () {
       if (xhttp.readyState === 3) {
-        var newData = xhttp.responseText.substr(seenBytes)
-        seenBytes = xhttp.responseText.length
-        response.insertAdjacentHTML('beforeEnd', newData)
-        response.scrollTop = response.scrollHeight - response.clientHeight
+        var newData = xhttp.responseText.substr(seenBytes);
+        seenBytes = xhttp.responseText.length;
+        response.insertAdjacentHTML('beforeEnd', newData);
+        response.scrollTop = response.scrollHeight - response.clientHeight;
       }
       if (xhttp.readyState === 4) {
-        response.insertAdjacentHTML('beforeEnd', '<br /><a href="javascript:location.reload()" class="button">CLOSE</a>')
-        response.scrollTop = response.scrollHeight - response.clientHeight
+        response.insertAdjacentHTML('beforeEnd', '<br /><a href="javascript:location.reload()" class="button">CLOSE</a>');
+        response.scrollTop = response.scrollHeight - response.clientHeight;
       }
-    }
-    xhttp.open('GET', element.getAttribute('data-xhr'), true)
-    xhttp.send()
+    };
+    xhttp.open('GET', element.getAttribute('data-xhr'), true);
+    xhttp.send();
   }
-}
-
-/**
- * build canvas for color picking
- */
-function buildCanvas() {
-  const img = document.querySelector('.image-container img.screenshot')
-  const canvas = document.querySelector('canvas#imgCanvas')
-  canvas.width = img.width
-  canvas.height = img.height
-  const canvasContext = canvas.getContext("2d")
-  canvasContext.fillStyle = "#FFFFFF"
-  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
-  canvasContext.drawImage(img, 0, 0)
-}
+};
 
 /**
  * toggle element by id
@@ -83,25 +68,25 @@ handler['data-click-color'] = {
   elements: [window],
   event: 'load',
   func: () => {
-    const element = document.querySelector('[data-click-color]')
-    const canvas = document.querySelector('canvas#imgCanvas')
+    const element = document.querySelector('[data-click-color]');
+    const canvas = document.querySelector('canvas#imgCanvas');
     if (canvas) {
-      const canvasContext = canvas.getContext("2d")
-      const colorOutput = document.querySelector(element.dataset.clickColor)
-      const positionOutput = document.querySelector('.mouse-position-output')
+      const canvasContext = canvas.getContext('2d');
+      const colorOutput = document.querySelector('.colorpicker-color');
+      const positionOutput = document.querySelector('.colorpicker-position');
       buildCanvas();
       element.addEventListener('click', (e) => {
         const rect = e.target.getBoundingClientRect();
-        const x = Math.round(e.clientX - rect.left); //x position within the element.
-        const y = Math.round(e.clientY - rect.top);  //y position within the element.
-        const pix = canvasContext.getImageData(x, y, 1, 1)
-        console.log(element.dataset.clickColor, x, y, pix.data[0], pix.data[1], pix.data[2], pix.data[3])
-        colorOutput.textContent = '#' + rgb2hex([pix.data[0], pix.data[1], pix.data[2]])
-        positionOutput.textContent = 'x:' + x + ', y:' + y
-      })
+        const x = Math.round(e.clientX - rect.left); // x position within the element.
+        const y = Math.round(e.clientY - rect.top); // y position within the element.
+        const pix = canvasContext.getImageData(x, y, 1, 1);
+        console.log(element.dataset.clickColor, x, y, pix.data[0], pix.data[1], pix.data[2], pix.data[3]);
+        colorOutput.textContent = '#' + rgb2hex([pix.data[0], pix.data[1], pix.data[2]]);
+        positionOutput.textContent = 'x:' + x + ', y:' + y;
+      });
     }
   }
-}
+};
 
 /**
  * attach event to elements
@@ -112,11 +97,11 @@ handler['data-click-color'] = {
  */
 function attachEventHandler (element, event, handler) {
   if (element.attachEvent) {
-    element.attachEvent('on' + event, handler)
+    element.attachEvent('on' + event, handler);
   } else if (element.addEventListener) {
-    element.addEventListener(event, handler, false)
+    element.addEventListener(event, handler, false);
   } else {
-    element.addEventListener(event, handler, false)
+    element.addEventListener(event, handler, false);
   }
 }
 
@@ -125,20 +110,34 @@ function attachEventHandler (element, event, handler) {
  */
 Object.values(handler).forEach((handler) => {
   handler.elements.forEach((element) => {
-    attachEventHandler(element, handler.event, handler.func)
-  })
-})
+    attachEventHandler(element, handler.event, handler.func);
+  });
+});
+
+/**
+ * build canvas for color picking
+ */
+function buildCanvas() {
+  const img = document.querySelector('.image-container img.screenshot');
+  const canvas = document.querySelector('canvas#imgCanvas');
+  canvas.width = img.width;
+  canvas.height = img.height;
+  const canvasContext = canvas.getContext('2d');
+  canvasContext.fillStyle = '#FFFFFF';
+  canvasContext.fillRect(0, 0, canvas.width, canvas.height);
+  canvasContext.drawImage(img, 0, 0);
+}
 
 function rgb2hex(colorArray) {
-  let hex = ''
+  let hex = '';
   colorArray.forEach((color) => {
-    console.log(color)
-    hex += componentToHex(color)
-  })
-  return hex.toUpperCase()
+    console.log(color);
+    hex += componentToHex(color);
+  });
+  return hex.toUpperCase();
 }
 
 function componentToHex(c) {
-  let hex = c.toString(16)
-  return hex.length === 1 ? '0' + hex : hex
+  let hex = c.toString(16);
+  return hex.length === 1 ? '0' + hex : hex;
 }
